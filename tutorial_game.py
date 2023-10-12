c385d6959ca7330dcf4b8ec9af12e67713409653
Zen <Zen2.Willetts@live.uwe.ac.uk>
2023-10-12 12:48:55 +0100

    Buttons added

--- tutorial_game.py
+++ tutorial_game.py
@@ -77,21 +77,41 @@ class MyASGEGame(pyasge.ASGEGame):
         #start button
         self.play_option = pyasge.Text(self.data.fonts["MainFont"])
         self.play_option.string = ">PLAY"
-        self.play_option.position = [500, 400]
+        self.play_option.position = [100, 400]
         self.play_option.colour = pyasge.COLOURS.HOTPINK
 
         #exit button
         self.exit_option = pyasge.Text(self.data.fonts["MainFont"])
         self.exit_option.string = "EXIT"
         self.exit_option.position = [500, 400]
-        self.exit_option.colour = pyasge.COLOURS.LIGHTGRAY
+        self.exit_option.colour = pyasge.COLOURS.LIGHTSLATEGRAY
 
         return True
     def clickHandler(self, event: pyasge.ClickEvent) -> None:
         pass
 
     def keyHandler(self, event: pyasge.KeyEvent) -> None:
-        pass
+
+        if event.action == pyasge.KEYS.KEY_PRESSED:
+
+            if event.key == pyasge.KEYS.KEY_RIGHT or event.key == pyasge.KEYS.KEY_LEFT:
+                self.menu_option = 1 - self.menu_option
+                if self.menu_option == 0:
+                    self.play_option.string = ">START"
+                    self.play_option.colour = pyasge.COLOURS.HOTPINK
+                    self.exit_option.string = " EXIT"
+                    self.exit_option.colour = pyasge.COLOURS.LIGHTSLATEGRAY
+                else:
+                    self.play_option.string = " START"
+                    self.play_option.colour = pyasge.COLOURS.LIGHTSLATEGRAY
+                    self.exit_option.string = ">EXIT"
+                    self.exit_option.colour = pyasge.COLOURS.HOTPINK
+
+        if event.key == pyasge.KEYS.KEY_ENTER:
+            if self.menu_option == 0:
+                self.menu = False
+            else:
+                self.signalExit()
 
     def spawn(self) -> None:
         pass
